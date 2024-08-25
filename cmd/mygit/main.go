49bbe5774ace884af16cc6a4186b463be6af0db0
Vikalp Gandha <vikalpgandha9712@gmail.com>
2024-08-25 23:24:58 +0530

    codecrafters submit [skip ci]

--- cmd/mygit/main.go
+++ cmd/mygit/main.go
@@ -21,6 +21,12 @@ func exitIfError(err error, msg string) {
 	}
 }
 
+type tree struct {
+	perm string
+	name string
+	sha  [20]byte
+}
+
 // Usage: your_program.sh <command> <arg1> <arg2> ...
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -114,6 +120,70 @@ func main() {
 		err = os.WriteFile(newFilePath, in.Bytes(), 0755)
 		exitIfError(err, "FILE WRITE")
 		os.Stdout.Write([]byte(hexhash))
+
+	case "ls-tree":
+		treeSha := os.Args[3]
+		// Create Path to tree in .git using treeSHA and Read the file
+		treePath := ".git/objects/" + treeSha[:2] + "/" + treeSha[2:]
+		data, err := os.ReadFile(treePath)
+		exitIfError(err, "READ_FILE")
+
+		// Decompress to raw using zlib
+		reader, err := zlib.NewReader(bytes.NewReader(data))
+		exitIfError(err, "ZLIB")
+		var out bytes.Buffer
+		io.Copy(&out, reader)
+		if !bytes.HasPrefix(out.Bytes(), []byte("tree ")) {
+			fmt.Println("Error: Not tree!")
+			os.Exit(1)
+		}
+
+		// Split all the data with zero-byte as seperator into chunks
+		chunks := bytes.Split(out.Bytes(), []byte{0})
+		var size int
+		processed := 0
+		ftree := tree{}
+		var trees []tree
+
+		// Loop through all the chunks to get all data of tree object
+		for i, chunk := range chunks {
+			if i == 0 {
+				// This will be header of the tree object.
+				// So we can get size of actual content from here.
+				size, err = strconv.Atoi(strings.Split(string(chunk), " ")[1])
+				exitIfError(err, "INVALID_SIZE")
+				continue
+			}
+			// Add size of actual content processed.
+			processed += len(chunk)
+			if i == 1 {
+				metadata := strings.Split(string(chunk), " ")
+				ftree.perm = metadata[0]
+				ftree.name = metadata[1]
+			} else {
+				// These chunks will contain zero-byte index
+				// which got removed in splitting process.
+				// So add 1 byte of that zero index as all those
+				// bytes are also part of content.
+				processed += 1
+				ftree.sha = [20]byte(chunk)
+				trees = append(trees, ftree)
+				ftree = tree{}
+
+				// Last chunk will not contain any metadata for next file
+				// so stop doing any process of all the bytes were only
+				// contained sha1 (last chunk)
+				if processed == size {
+					break
+				}
+				metadata := strings.Split(string(chunk[20:]), " ")
+				ftree.perm = metadata[0]
+				ftree.name = metadata[1]
+			}
+		}
+		for _, t := range trees {
+			fmt.Println(t.name)
+		}
 
 	default:
 		fmt.Fprintf(os.Stderr, "Unknown command %s\n", command)
